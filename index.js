34bfcc11625c212c820f3dce1a0ce55eab1aea5f
frankly online <frankmindolo@gmail.com>
2025-06-02 15:33:33 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,33 @@
+const express = require("express");
+const app = express();
+const cors = require('cors');
+const { createProxyMiddleware } = require('http-proxy-middleware');
+
+// app.use(express.urlencoded({
+//   extended: true
+// }));
+
+// app.use(cors());
+// app.use(express.json())
+const PORT = 4900;
+
+let target = 'http://82.112.240.121:4000'
+target = 'http://localhost:4000'
+// Middleware for download audio requests
+app.use('/file', createProxyMiddleware({
+  target , // Target for the first app
+  changeOrigin: true,
+  pathRewrite: {
+      '^/file': '', // Remove the /api/backend-social-poster prefix
+  },
+}));
+
+
+app.get('/',(req,res)=>{
+res.send('Proxy Middleware Server is online!').end()
+})
+
+
+app.listen(PORT, () => {
+ console.log(`Listening on port ${PORT}`);
+});
