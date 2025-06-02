7a07b9dbc7382d0d0fa83b2bb12fc3eac1938535
frankly online <frankmindolo@gmail.com>
2025-06-02 18:14:13 +0300

    target chnge

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ app.use(express.json())
 const PORT = 4900;
 
 let target = 'http://82.112.240.121:4000'
-target = 'http://localhost:4000'
+// target = 'http://localhost:4000'
 // Middleware for download audio requests
 app.use('/audsvr', createProxyMiddleware({
   target , // Target for the first app
